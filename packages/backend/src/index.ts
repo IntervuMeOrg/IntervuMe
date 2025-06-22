d19d6a4311f8334c0d548babc19202c26c1d1770
Karim Hassib <karimhassib537@gmail.com>
2025-06-22 12:50:16 +0000

    fix: separate jwt from index, created plugin

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata";
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 import cors from "@fastify/cors";
-import jwt from "@fastify/jwt";
 import { config } from "dotenv";
 import { AppDataSource } from "./database/data-source.js";
 import { authenticationController } from "./auth/auth.controller.js";
 import { userController } from "./user/user.controller.js";
+import jwtPlugin from "./plugins/jwt";
 
 config();
 
@@ -13,19 +13,7 @@ const fastify = Fastify({
   logger: true,
 });
 
-// Register JWT
-await fastify.register(jwt, {
-  secret: process.env.JWT_SECRET || "your-secret-key-change-in-production",
-  sign: { expiresIn: "24h" },
-});
-
-fastify.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
-  try {
-    await request.jwtVerify();
-  } catch {
-    reply.code(401).send({ success: "false", message: "unauthorized" });
-  }
-});
+fastify.register(jwtPlugin)
 
 // Register CORS
 await fastify.register(cors, {
