de282d0bf307209009087dd7d60aad70541fdc19
Gamal72 <gamalfcai@gmail.com>
2025-06-24 21:31:53 +0000

    feat: add ui configuration

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,97 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "app/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "text-3xl-leading-9-font-medium":
+          "var(--text-3xl-leading-9-font-medium-font-family)",
+        "text-base-leading-6-font-medium":
+          "var(--text-base-leading-6-font-medium-font-family)",
+        "text-base-leading-6-font-normal":
+          "var(--text-base-leading-6-font-normal-font-family)",
+        "text-base-leading-6-font-semibold":
+          "var(--text-base-leading-6-font-semibold-font-family)",
+        "text-lg-leading-7-font-bold":
+          "var(--text-lg-leading-7-font-bold-font-family)",
+        "text-tiny": "var(--text-tiny-font-family)",
+        "text-xl-leading-7-font-bold":
+          "var(--text-xl-leading-7-font-bold-font-family)",
+        "text-xs-leading-4-font-bold":
+          "var(--text-xs-leading-4-font-bold-font-family)",
+        "text-xs-leading-4-font-medium":
+          "var(--text-xs-leading-4-font-medium-font-family)",
+        sans: [
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+      boxShadow: { "shadow-neutral-sm": "var(--shadow-neutral-sm)" },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+    container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
+  },
+  plugins: [],
+  darkMode: ["class"],
+};
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import tailwind from "tailwindcss";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  }
-}) 
+  base: "./",
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
+  },
+});
