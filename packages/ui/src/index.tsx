3792d07efdb8cdf281e8430ca21c8c62251f90b7
Gamal72 <gamalfcai@gmail.com>
2025-06-24 21:44:06 +0000

    feat: ui main index

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MainPage } from "./screens/MainPage";
+import { LandingPage } from "./screens/LandingPage";
 import { LoginPage } from "./screens/LoginPage";
 import { RegistrationPage } from "./screens/RegistrationPage";
 import { ForgetPasswordPage } from "./screens/ForgetPasswordPage";
-import { CreateNewPasswordPage } from "./screens/CreateNewPassword";
-import { MainPageAfterLogin } from "./screens/MainPageAfterLogin";
+import { CreateNewPasswordPage } from "./screens/CreateNewPasswordPage";
+import { AppPage } from "./screens/AppPage";
 import { HistoryPage } from "./screens/HistoryPage";
 import { ProfilePage } from "./screens/ProfilePage";
 import { StartInterviewPage } from "./screens/StartInterviewPage";
@@ -19,7 +19,7 @@ createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/forget-password" element={<ForgetPasswordPage />} />
@@ -29,7 +29,7 @@ createRoot(document.getElementById("app") as HTMLElement).render(
           element={<CreateNewPasswordPage />}
         />
 
-        <Route path="/main-page-after-login" element={<MainPageAfterLogin />} />
+        <Route path="/main-page-after-login" element={<AppPage />} />
         <Route path="/history" element={<HistoryPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/settings" element={<SettingsPage />} />
